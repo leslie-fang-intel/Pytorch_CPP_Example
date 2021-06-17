b79b3e0b71cc6bf9749a3d6ba66ffbd1443055da
leslie-fang-intel <leslie.fang@intel.com>
2021-06-18 06:06:13 +0800

    enable logs

--- example-app.cpp
+++ example-app.cpp
@@ -1,6 +1,8 @@
 #include <torch/torch.h>
 #include <torch/script.h>
+#include <c10/util/Exception.h>
 #include <iostream>
+#include <omp.h>
 
 int main()
 {
@@ -9,7 +11,8 @@ int main()
   // Load values by name
   torch::Tensor batch_scores = container.attr("scores").toTensor();
 
-  std::cout<<"at::nonzero(score > 0.05): "<<at::nonzero(batch_scores[15].squeeze(0).slice(1, 1, 2).squeeze(1) > 0.05).sizes()<<std::endl;
+  auto compare_result = at::nonzero(batch_scores[15].squeeze(0).slice(1, 1, 2).squeeze(1) > 0.05).squeeze(1);
+  std::cout<<"single thread result at BS 16 and class 1: "<<compare_result.sizes()<<std::endl;
 
   auto nbatch = batch_scores.size(0); // number of batches 16
   auto ndets = batch_scores.size(1); // number of boxes 15130
@@ -17,31 +20,30 @@ int main()
 
   auto nbatch_x_nscore = nbatch * nscore; // (number of batches) * (number of labels)
 
-#ifdef _OPENMP
-#if (_OPENMP >= 201307)
-# pragma omp parallel for simd schedule(static) if (omp_get_max_threads() > 1 && !omp_in_parallel())
-#else
-# pragma omp parallel for schedule(static) if (omp_get_max_threads() > 1 && !omp_in_parallel())
-#endif
-#endif
-  for (int index = 0; index < nbatch_x_nscore; index++) {
-    auto bs = index / nscore;
-    auto i = index % nscore;
-    at::Tensor scores = batch_scores[bs].squeeze(0); // scores for boxes per image: (num_bbox, 81); For example: (15130, 81)
+  //at::set_num_threads(at::intraop_default_num_threads());
+  int64_t grain_size = std::min(at::internal::GRAIN_SIZE / nbatch_x_nscore, (int64_t)1);
+  at::parallel_for(0, nbatch_x_nscore, grain_size, [&](int64_t begin, int64_t end){
+    for (int index = begin; index < end; index++) {
+      //omp_set_num_threads(28);
+      auto bs = index / nscore;
+      auto i = index % nscore;
+      at::Tensor scores = batch_scores[bs].squeeze(0); // scores for boxes per image: (num_bbox, 81); For example: (15130, 81)
+      at::Tensor score = scores.slice(1, i, i+1).squeeze(1); // score for boxes per image per class: (num_bbox); For example: (15130)
 
-    at::Tensor score = scores.slice(1, i, i+1).squeeze(1); // score for boxes per image per class: (num_bbox); For example: (15130)
+      at::Tensor mask_index = at::nonzero(score > 0.05).squeeze(1);
 
-    if (bs == 15 && i == 1) {
-      std::cout<<"score: "<<score.sizes()<<std::endl;
-      std::cout<<"score > 0.05: "<<(score > 0.05).sizes()<<std::endl;
-      std::cout<<"at::nonzero(score > 0.05): "<<at::nonzero(score > 0.05).sizes()<<std::endl;
+      if (bs == 15 && i == 1) {
+        std::cout<<"Multi threads result at BS 16 and class 1: "<<mask_index.sizes()<<std::endl;
+        auto pointer1 = compare_result.data<int64_t>();
+        auto pointer2 = mask_index.data<int64_t>();
+        for(int j =0; j<mask_index.sizes()[0];j++ ){
+          std::cout<<pointer1[j]<<std::endl;
+          std::cout<<pointer2[j]<<std::endl;
+        }
+        
+      }
     }
+  });
 
-    at::Tensor mask_index = at::nonzero(score > 0.05).squeeze(1);
-
-    if (bs == 15 && i == 1) {
-      std::cout<<"mask_index: "<<mask_index.sizes()<<std::endl;
-    }
-  }
   return 0;
 }
